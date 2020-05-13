316a50a569d003de60c2c9f00b2d04be8401d527
Nikhil Balwani <nikhilbalwani1998@gmail.com>
2020-05-13 23:56:27 +0530

    Comments

--- driver.py
+++ driver.py
@@ -1,12 +1,15 @@
 from xor_linked_list import Node
 
-
+# Creating three different nodes
 node1 = Node(data=1)
 node2 = Node(data=2)
 node3 = Node(data=3)
 
+# Precede node1 by None and succeed by node2 
 node1.set_neighbors(prev_node=None, next_node=node2)
+# Precede node2 by node1 and succeed by node3
 node2.set_neighbors(prev_node=node1, next_node=node3)
+# Precede node3 by node2 and succeed by None
 node3.set_neighbors(prev_node=node2, next_node=None)
 
 curr_node = node1
